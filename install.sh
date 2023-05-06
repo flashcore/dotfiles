0c6fb84700f86be1f1c7344fdddfcb34d2495507
flashcore <56236544+flashcore@users.noreply.github.com>
2023-05-06 13:56:28 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,5 +7,5 @@ sudo chmod +x /usr/local/bin/yq
 for x in .aliases .bash_profiles .bash_prompt .bashrc .curlrc .dockerfunctions .exports .functions .path .screenrc .k8s;
 do
   echo "[*] Downloading to $HOME/$x"
-  curl -qo $HOME/$x https://raw.githubusercontent.com/veerendra2/dotfiles/master/$x > /dev/null 2>&1
+  curl -qo $HOME/$x https://raw.githubusercontent.com/flashcore/dotfiles/master/$x > /dev/null 2>&1
 done
